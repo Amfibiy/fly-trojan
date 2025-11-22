c64b9f6f3922d42f17f454959a44e324d392a6ba
Amfibiy <syropyatov.arseniy@gmail.com>
2025-11-22 18:16:01 +0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-# Запускаем HTTP-сервер в фоне
 lighttpd -f /etc/lighttpd/lighttpd.conf
 
 # Ждём, пока порт 80 станет доступен
@@ -7,5 +6,4 @@ while ! nc -z 127.0.0.1 80; do
   sleep 0.5
 done
 
-# Теперь запускаем Trojan-Go в foreground (PID 1)
 exec trojan-go -config /etc/trojan/config.json
