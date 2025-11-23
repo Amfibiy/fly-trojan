eb84b048257a5f14a14e6b24a1565b10dde9b186
Amfibiy <syropyatov.arseniy@gmail.com>
2025-11-23 21:19:20 +0500

    Delete start.sh

--- start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-lighttpd -f /etc/lighttpd/lighttpd.conf
-
-# Ждём, пока порт 80 станет доступен
-while ! nc -z 127.0.0.1 80; do
-  sleep 0.5
-done
-
-exec trojan-go -config /etc/trojan/config.json
